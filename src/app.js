a5416d42f5029f9e6146450104335876b55eb394
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-16 15:27:56 -0500

    api gateway message processed better

--- src/app.js
+++ src/app.js
@@ -47,7 +47,10 @@ app.get('/ping', async (_, res) => {
     res.status(200).json(results);
 });
 app.use('/checkBalance', balance);
-app.post('/api-gateway', async (_, res) => res.status(200).json(await apiGateway()));
+app.post('/api-gateway', async (_, res) => {
+    const { status, message, error } = await apiGateway();
+    res.status(status).json(message, error);
+});
 app.use((_, res) => res.status(404).json({ message: 'Not Found' }));
 
 export default app;
